68b27266d7df5888c1fa1066b41ad2da73fdf47f
Andrea Fonti <andreafontict@gmail.com>
2012-08-17 20:51:45 +0200

    Working TrayIcon to manage jarvis service

---
+++ Jarvis.TrayClient/NegatingConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Jarvis.TrayClient
+{
+
+public class NegatingConverter : IValueConverter
+{
+    #region Implementation of IValueConverter
+
+    /// <summary>
+    /// Converte un valore. 
+    /// </summary>
+    /// <returns>
+    /// Un valore convertito.Se il metodo restituisce null, viene utilizzato il valore null valido.
+    /// </returns>
+    /// <param name="value">Valore prodotto dall'origine dell’associazione.</param><param name="targetType">Tipo della proprietà origine dell’associazione.</param><param name="parameter">Il parametro del convertitore da utilizzare.</param><param name="culture">Le impostazioni cultura da utilizzare nel convertitore.</param>
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value!= null)
+        {
+            return !((bool)value);    
+        }
+        
+        return null;
+        
+        
+    }
+
+    /// <summary>
+    /// Converte un valore. 
+    /// </summary>
+    /// <returns>
+    /// Un valore convertito.Se il metodo restituisce null, viene utilizzato il valore null valido.
+    /// </returns>
+    /// <param name="value">Valore prodotto dalladestinazione dell’associazione.</param><param name="targetType">Tipo in cui eseguire la conversione.</param><param name="parameter">Il parametro del convertitore da utilizzare.</param><param name="culture">Le impostazioni cultura da utilizzare nel convertitore.</param>
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return Convert(value, targetType, parameter, culture);
+    }
+
+    #endregion
+}
+}
---
+++ Jarvis.TrayClient/TrayPopupUserControl.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Jarvis.TrayClient
+{
+    /// <summary>
+    /// Logica di interazione per TrayPopupUserControl.xaml
+    /// </summary>
+    public partial class TrayPopupUserControl : UserControl, INotifyPropertyChanged
+    {
+        private DispatcherTimer _timer;
+        private ServiceController _serviceController;
+
+
+        private Boolean _serviceIsRunning;
+        public Boolean ServiceIsRunning
+        {
+            get { return _serviceIsRunning; }
+            set
+            {
+                if (_serviceIsRunning != value)
+                {
+                    _serviceIsRunning = value;
+                    OnPropertyChanged((x) => x.ServiceIsRunning);
+                }
+            }
+        }
+
+        private SolidColorBrush _labelBrush = new SolidColorBrush();
+        public SolidColorBrush LabelBrush
+        {
+            get { return _labelBrush; }
+            set
+            {
+                if (_labelBrush != value)
+                {
+                    var ca = new ColorAnimation(value.Color, new Duration(TimeSpan.FromMilliseconds(200)));
+                    _labelBrush.BeginAnimation(SolidColorBrush.ColorProperty, ca);
+                    OnPropertyChanged((x) => x.LabelBrush);
+                }
+            }
+        }
+
+        private SolidColorBrush _labelBorderBrush = new SolidColorBrush();
+        public SolidColorBrush LabelBorderBrush
+        {
+            get { return _labelBorderBrush; }
+            set
+            {
+                var ca = new ColorAnimation(value.Color, new Duration(TimeSpan.FromMilliseconds(200)));
+                _labelBorderBrush.BeginAnimation(SolidColorBrush.ColorProperty, ca);
+                OnPropertyChanged((x) => x.LabelBorderBrush);
+            }
+        }
+
+        public TrayPopupUserControl()
+        {
+            InitializeComponent();
+            _serviceController = new ServiceController();
+            _timer = new DispatcherTimer
+                         {
+                             Interval = TimeSpan.FromSeconds(1),
+                             IsEnabled = true
+                         };
+            _timer.Tick += UpdateServiceStatus;
+            _timer.Start();
+            LabelBrush = new SolidColorBrush(Colors.Red);
+            LabelBorderBrush = new SolidColorBrush(Colors.Red);
+            ServiceLabel.Content = "Jarvis Service is not installed";
+
+        }
+
+        void UpdateServiceStatus(object sender, EventArgs args)
+        {
+            _serviceController =
+                ServiceController.GetServices().FirstOrDefault(service => service.ServiceName.Contains("Jarvis"));
+
+            if (_serviceController == null)
+            {
+                ServiceLabel.Content = "Jarvis Service is not installed";
+            }
+            else
+            {
+
+                if (_serviceController.Status == ServiceControllerStatus.Running)
+                {
+                    ServiceLabel.Content = "Service Started";
+                    LabelBrush = new SolidColorBrush(Colors.Green);
+                    LabelBorderBrush = new SolidColorBrush(Colors.DarkGreen);
+                    ServiceIsRunning = true;
+                }
+                else
+                {
+                    ServiceLabel.Content = "Service Stopped";
+                    LabelBrush = new SolidColorBrush(Colors.Red);
+                    LabelBorderBrush = new SolidColorBrush(Colors.DarkRed);
+                    ServiceIsRunning = false;
+                }
+            }
+        }
+
+
+
+        private void StartButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (_serviceController != null)
+            {
+                try
+                {
+                    _serviceController.Start();
+                }
+                catch (InvalidOperationException ex)
+                {
+                    ServiceLabel.Content = "Error";
+                    LabelBrush = new SolidColorBrush(Colors.Red);
+                    LabelBorderBrush = new SolidColorBrush(Colors.DarkRed);
+                }
+            }
+        }
+
+        private void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (_serviceController != null)
+            {
+                _serviceController.Close();
+            }
+
+            _timer.Stop();
+
+            Application.Current.Shutdown();
+        }
+
+        private void StopButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (_serviceController != null)
+            {
+                try
+                {
+                    _serviceController.Stop();
+                }
+                catch (InvalidOperationException ex)
+                {
+                    ServiceLabel.Content = "Error";
+                    LabelBrush = new SolidColorBrush(Colors.Red);
+                    LabelBorderBrush = new SolidColorBrush(Colors.DarkRed);
+                }
+            }
+        }
+
+        #region Implementation of INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(Expression<Func<TrayPopupUserControl, object>> property)
+        {
+            string propertyName = null;
+            MemberExpression memberExp = property.Body as MemberExpression;
+            if (memberExp != null)
+                 propertyName = memberExp.Member.Name;
+
+            // for DateTime
+            UnaryExpression unaryExp = property.Body as UnaryExpression;
+            if (unaryExp != null)
+            {
+                memberExp = unaryExp.Operand as MemberExpression;
+                if (memberExp != null)
+                    propertyName= memberExp.Member.Name;
+            }
+
+            var eventArgs = new PropertyChangedEventArgs(propertyName);
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, eventArgs);
+        }
+
+        #endregion
+    }
+}
